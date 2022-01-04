18226fdb19c9b41cbf45a54e6254222c0f066bed
jlb14n <jlb14n@gmail.com>
2022-01-04 17:16:12 -0500

    Created file

---
+++ DataClasses.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class Orders:
+    pass
+
+    def __gt__(self,other):
+        pass
+
+    def __ge__(self,other):
+        pass
+
+@dataclass
+class Invoices:
+    pass
+
+    def __gt__(self,other):
+        pass
+
+    def __ge__(self,other):
+        pass
+
+@dataclass
+class Customers:
+    pass
