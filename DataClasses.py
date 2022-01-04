ed1652b880f978699e663933c57097c644275c0a
jlb14n <jlb14n@gmail.com>
2022-01-04 18:53:50 -0500

    Completed Data Class

--- DataClasses.py
+++ DataClasses.py
@@ -1,25 +1,80 @@
 from dataclasses import dataclass, field
+import datetime
 
 @dataclass
 class Orders:
-    pass
-
+    """Orders Class, fields taken from Sales.OrderLines"""
+    OrderLineID: int
+    OrderID: int
+    StockItemID: int
+    Description: str
+    PackageTypeID: int
+    Quantity: int
+    UnitPrice: float = field(metatdata={"units":"$"})
+    TaxRate: float = field(metadata={"units":"%"})
+    PickedQuantity: int
+    PickingCompletedWhen: datetime
+    LastEditedBy: int
+    LastEditedWhen: datetime
     def __gt__(self,other):
-        pass
+        return self.__quantity*self.__UnitPrice*(1+self.__TaxRate/100)>other.__quantity*other.__UnitPrice*(1+other.__TaxRate/100)
 
     def __ge__(self,other):
-        pass
+        return self.__quantity*self.__UnitPrice*(1+self.__TaxRate/100)>=other.__quantity*other.__UnitPrice*(1+other.__TaxRate/100)
 
 @dataclass
 class Invoices:
-    pass
-
+    """Invoices class, fields taken from Sales.InvoiceLines"""
+    InvoiceLineID: int
+    InvoiceID: int
+    StockItemID: int
+    Description: str
+    PackageTypeID: int
+    Quantity: int
+    UnitPrice: float = field(metadata={"units":"$"})
+    TaxRate: float = field(metadata={"units":"%"})
+    TaxAmount: float = field(metadata={"units":"$"})
+    LineProfit: float = field(metadata={"units":"$"})
+    ExtendedPrice: float = field(metadata={"units":"$"})
+    LastEditedBy: int
+    LastEditedWhen: datetime
     def __gt__(self,other):
-        pass
+        self.__ExtendedPrice>other.__ExtendedPrice
 
     def __ge__(self,other):
-        pass
+        self.__ExtendedPrice>=other.__ExtendedPrice
 
 @dataclass
 class Customers:
-    pass
+    """Customers class, fields taken from Sales.Customers"""
+    CustomerID: int
+    CustomerName: str
+    BillToCustomerID: int
+    CustomerCategoryID: int
+    BuyingGroupID: int
+    PrimaryContactPersonID: int
+    AlternateContactPersonID: int
+    DeliveryMethodID: int
+    DeliveryCityID: int
+    PostalCityID: int
+    CreditLimit: float = field(metadata={"units":"$"})
+    AccountOpenedDate: str
+    StandardDiscountPercentage: float = field(metadata={"units":"%"})
+    IsStatementSent: bool
+    IsOnCreditHold: bool
+    PaymentDays: int
+    PhoneNumber: str
+    FaxNumber: str
+    DeliveryRun: str
+    RunPosition: str
+    WebsiteURL: str
+    DeliveryAddressLine1: str
+    DeliveryAddressLine2: str
+    DeliveryPostalCode: str
+    DeliveryLocation: str
+    PostalAddressLine1: str
+    PostalAddressLine2: str
+    PostalPostalCode: str
+    LastEditedBy: int
+    ValidFrom: datetime
+    ValidTo: datetime
